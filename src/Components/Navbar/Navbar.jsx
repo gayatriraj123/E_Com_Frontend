bc2dc2be2a6f5bbae7c6c0dc5b780f117070e6be
gayatriraj123 <gayatrirajguru2002@gmail.com>
2024-01-20 12:35:59 +0530

    frontend commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import "./Navbar.css";
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
+import dropDown from '../Assets/dropDown.png';
 
 
 const Navbar = () => {
 
     const [menu,setmenu] = useState("shop")
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+    const dropDown_toggle = (e) =>{
+      menuRef.current.classList.toggle('nav-menu-visible');
+      e.target.classList.toggle('open');
+    }
 
   return (
     <div className='navbar'>
@@ -18,17 +24,19 @@ const Navbar = () => {
       {/* <p>SHREEMATI<br/>COLLECTION</p> */}
       <p>SHREEMATI</p>
       </div>
-      <ul className='nav-menu'>
+      <img className='nav-dropdown' onClick={dropDown_toggle} src={dropDown} alt= '' />
+      <ul ref={menuRef} className='nav-menu'>
       <li onClick={()=>{setmenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
       <li onClick={()=>{setmenu("mens")}}><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
       <li onClick={()=>{setmenu("womens")}}><Link style={{textDecoration: 'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
       <li onClick={()=>{setmenu("kids")}}><Link style={{textDecoration: 'none'}} to='/kids'>kids</Link>{menu==="kids"?<hr/>:<></>}</li>
       </ul>
         <div className='nav-login-cart'>
-        <Link to='/login'><button>Login</button></Link>
+        {localStorage.getItem('auth-token')
+        ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+        :<Link to='/login'><button>Login</button></Link>}
         <Link to='/cart'><img src={cart_icon} alt='' /></Link>
         <div className='nav-cart-count'>{getTotalCartItems()}</div>
-
         </div>
     </div>
   );
--- src/Components/Newsletter/NewsLetter.jsx
+++ src/Components/Newsletter/NewsLetter.jsx
@@ -5,7 +5,7 @@ const NewsLetter = () => {
   return (
     <div className='newsletter'>
     <h1>Get Exclusive Offer On Your Email</h1>
-    <p>Subscribe to uor newsletter and stay updated</p>
+    <p>Subscribe to our newsletter and stay updated</p>
     <div>
         <input type='email' placeholder='Your Email Id' />
         <button>Subscribe</button>
